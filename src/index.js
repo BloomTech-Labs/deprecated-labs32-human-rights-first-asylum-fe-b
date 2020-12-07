d077597a289432480ebe48acf01d03f418ed8d68
David Gold <davidgold@hey.com>
2020-12-07 10:16:28 -0500

    page skeleton

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import { LandingPage } from './components/pages/Landing';
 import { ExampleDataViz } from './components/pages/ExampleDataViz';
 import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
+import { Dashboard } from './components/pages/Dashboard';
 
 ReactDOM.render(
   <Router>
@@ -43,11 +44,12 @@ function App() {
   return (
     <Security {...config} onAuthRequired={authHandler}>
       <Switch>
-        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={Dashboard} />
+        {/* <Route path="/login" component={LoginPage} />
         <Route path="/implicit/callback" component={LoginCallback} />
-        <Route path="/landing" component={LandingPage} />
+        <Route path="/landing" component={LandingPage} /> */}
         {/* any of the routes you need secured should be registered as SecureRoutes */}
-        <SecureRoute
+        {/* <SecureRoute
           path="/"
           exact
           component={() => <HomePage LoadingComponent={LoadingComponent} />}
@@ -55,7 +57,7 @@ function App() {
         <SecureRoute path="/example-list" component={ExampleListPage} />
         <SecureRoute path="/profile-list" component={ProfileListPage} />
         <SecureRoute path="/datavis" component={ExampleDataViz} />
-        <Route component={NotFoundPage} />
+        <Route component={NotFoundPage} /> */}
       </Switch>
     </Security>
   );
