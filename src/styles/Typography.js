c017e9163d367b20f8afa222dbb6040d29e118da
David Gold <davidgold@hey.com>
2020-12-09 12:48:46 -0500

    Added a Global Styles file for Typography (Typography.js)

---
+++ src/styles/Typography.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Typography = createGlobalStyle`
+    html {
+        font-family: Helvetica, Arial, sans-serif;
+        color: #1f1f1f;
+    }
+`;
+
+export default Typography;
