6720c13beccfe40cb73a3ee499fbe04d0bdbb411
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-12-14 10:01:01 -0500

    added a new file to allow for styling of the LoginContainer, and also used it to customize the page's styling

---
+++ src/components/pages/Login/LoginContainer-styling.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledLogin = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .background-image {
+    background-image: url('https://via.placeholder.com/150?text=Placeholder');
+    width: 50vw;
+    height: 88vh;
+  }
+
+  #sign-in-widget {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    padding-bottom: 20%;
+  }
+`;
