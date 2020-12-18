6aeee5aa1c07ec1ec43defd032ffdbc487daf42e
David Gold <65919583+davidgoldcode@users.noreply.github.com>
2020-12-18 14:45:28 -0500

    Delete oktaConfig.js

--- src/utils/oktaConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  issuer: 'https://dev-625244.okta.com/outh2/default',
-  redirectUri: window.location.origin + '/implicit/callback',
-  clientId: '0oauwlyl5Xf6AvJxv4x6',
-  pkce: true,
-  scopes: ['openid', 'email', 'profile'],
-};
-
-export { config };
