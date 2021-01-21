b032bd0d3b0d26cdbc289e8427b9cd2e03500b0c
amichalo421 <andre.michalowski@gmail.com>
2021-01-21 18:34:47 -0500

    Antd cascading-drop-down generic added

--- src/components/common/Header/Collections.js
+++ src/components/common/Header/Collections.js
@@ -1,2 +1,37 @@
 //"Cascading Drop-down Menu" for bookmarks functionality.
 // (To be added to index.js once header styling completed.)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+const menu = (
+  <Menu>
+    <Menu.ItemGroup title="Group title">
+      <Menu.Item>1st menu item</Menu.Item>
+      <Menu.Item>2nd menu item</Menu.Item>
+    </Menu.ItemGroup>
+    <SubMenu title="sub menu">
+      <Menu.Item>3rd menu item</Menu.Item>
+      <Menu.Item>4th menu item</Menu.Item>
+    </SubMenu>
+    <SubMenu title="disabled sub menu" disabled>
+      <Menu.Item>5d menu item</Menu.Item>
+      <Menu.Item>6th menu item</Menu.Item>
+    </SubMenu>
+  </Menu>
+);
+
+ReactDOM.render(
+  <Dropdown overlay={menu}>
+    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+      Cascading menu <DownOutlined />
+    </a>
+  </Dropdown>,
+  document.getElementById('container')
+);
